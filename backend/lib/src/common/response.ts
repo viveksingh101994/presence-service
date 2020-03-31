6bec4ba908b045e4d1711174450ae9b20ccdc9fa
viveksinghscheduleonce <viveksingh101994@gmail.com>
2020-04-01 01:41:58 +0530

    fixed for prettier

--- backend/lib/src/common/response.ts
+++ backend/lib/src/common/response.ts
@@ -3,43 +3,43 @@ export let Response = {
     status: 403,
     message: {
       type: 'invalid_request_error',
-      message: 'Not authorized',
-    },
+      message: 'Not authorized'
+    }
   },
   UserAlreadyExist: {
     status: 400,
     message: {
       type: 'invalid_request_error',
-      message: 'User already exist',
-    },
+      message: 'User already exist'
+    }
   },
   FireBase: {
     status: 400,
-    message: {},
+    message: {}
   },
   InvalidParam: {
     status: 400,
     message: {
       type: 'invalid_request_error',
-      message: 'Invalid value of parameter',
-    },
+      message: 'Invalid value of parameter'
+    }
   },
   UserNotFound: {
     status: 404,
     message: {
       type: 'invalid_user_error',
-      message: 'User Not Found',
-    },
+      message: 'User Not Found'
+    }
   },
   ServerError: {
     status: 500,
     message: {
       type: 'api_error',
-      message: 'Internal server error',
-    },
+      message: 'Internal server error'
+    }
   },
   Success: {
     status: 200,
-    message: {},
-  },
+    message: {}
+  }
 };
