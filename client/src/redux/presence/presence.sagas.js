852f46140a705f2dab184e811de6507809e8a9b7
viveksinghscheduleonce <viveksingh101994@gmail.com>
2020-04-02 03:25:13 +0530

    fixing the condition

--- client/src/redux/presence/presence.sagas.js
+++ client/src/redux/presence/presence.sagas.js
@@ -1,11 +1,11 @@
-import { takeLatest, put, all, call } from "redux-saga/effects";
-import { PresenceActionTypes } from "./presence.types";
-import { post } from "../../axios/axios.utiils";
-import { roomUserUrl } from "../../global.config";
-import { cookies } from "../../cookie/cookie.name";
-import { getCookie } from "../../cookie/cookie.util";
-import { userPresentSuccess, setPresentUsersFailure } from "./presence.actions";
-import { signOutSuccess } from "../user/user.actions";
+import { takeLatest, put, all, call } from 'redux-saga/effects';
+import { PresenceActionTypes } from './presence.types';
+import { post } from '../../axios/axios.utiils';
+import { roomUserUrl } from '../../global.config';
+import { cookies } from '../../cookie/cookie.name';
+import { getCookie } from '../../cookie/cookie.util';
+import { userPresentSuccess, setPresentUsersFailure } from './presence.actions';
+import { signOutSuccess } from '../user/user.actions';
 
 export function* onGetPresentUsersStart() {
   yield takeLatest(PresenceActionTypes.GET_PRESENT_USERS_START, getUserPresent);
@@ -13,17 +13,20 @@ export function* onGetPresentUsersStart() {
 
 export function* getUserPresent({ payload }) {
   try {
-    if (getCookie(cookies.validToken) === "true") {
+    if (getCookie(cookies.validToken) === 'true') {
       if (payload.occupants.length === 1) {
         return yield put(userPresentSuccess({ user: [payload.user] }));
+      } else {
+        const userList = payload.occupants
+          .filter((x) => x.uuid !== payload.user.uid)
+          .map((x) => x.uuid);
+        const userData = yield post(roomUserUrl(), {
+          payload: userList
+        });
+        yield put(
+          userPresentSuccess({ user: [payload.user, ...userData.data] })
+        );
       }
-      const userList = payload.occupants
-        .filter(x => x.uuid !== payload.user.uid)
-        .map(x => x.uuid);
-      const userData = yield post(roomUserUrl(), {
-        payload: userList
-      });
-      yield put(userPresentSuccess({ user: [payload.user, ...userData.data] }));
     } else {
       yield put(signOutSuccess());
     }
